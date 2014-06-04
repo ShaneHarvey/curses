44879d63dc3210799b17e5f129b9b5037e5be3cf
Shane Harvey <shane6harvey@yahoo.com>
2014-06-04 03:48:10 -0400

    Changed to 2d array

--- clife.c
+++ clife.c
@@ -9,13 +9,11 @@
 #define HEIGHT (LINES-1)
 #define DELAY 75001
 
-char *grid;
-
 /*
  * The neighbors are the 8 cells around (x,y) that are on.
  * Remember the grid is toroidal, it wraps around on all sides.
  */
-int num_neighbors(int x, int y){
+int num_neighbors(char grid[][HEIGHT], int x, int y){
 	int count = 0, i, j;
 	if(x == 0)
 		i = WIDTH - 1;
@@ -27,45 +25,44 @@ int num_neighbors(int x, int y){
 		else
 			j = y - 1;
 		for(; j != (y+2)%HEIGHT; j = (j+1)%HEIGHT){
-			if(grid[i + WIDTH*j] == ON)
+			if(grid[i][j] == ON)
 				count++;
 		}
 	}
 	// Uncount the cell (x,y)
-	if(grid[x + WIDTH*y] == ON)
+	if(grid[x][y] == ON)
 		return count - 1;
 	return count;
 }
 
-void update_grid(){
+void update_grid(char grid[][HEIGHT]){
 	int x, y;
 	// Make a temporary grid to hold the new states
-	char *temp_grid = (char *)malloc(WIDTH*HEIGHT);
+	char temp_grid[WIDTH][HEIGHT];
 	for(x = 0; x < WIDTH; ++x){
 		for(y = 0; y < HEIGHT; ++y){
-			switch(num_neighbors(x, y)){
+			switch(num_neighbors(grid, x, y)){
 				case 2:
-					temp_grid[x + WIDTH*y] = grid[x + WIDTH*y];
+					temp_grid[x][y] = grid[x][y];
 					break;
 				case 3:
-					temp_grid[x + WIDTH*y] = ON;
+					temp_grid[x][y] = ON;
 					break;
 				default:
-					temp_grid[x + WIDTH*y] = OFF;
+					temp_grid[x][y] = OFF;
 			}
 		}
 	}
 	// Copy temp_grid into grid
-	strncpy(grid, temp_grid, HEIGHT*WIDTH);
-	free(temp_grid);
+	memcpy(grid, temp_grid, HEIGHT*WIDTH);
 }
 
-void display(const char *info){
+void display(char grid[][HEIGHT], const char *info){
 	int x, y;
 	clear();
 	for(y = 0; y < HEIGHT; ++y){
 		for(x = 0; x < WIDTH; ++x){
-			mvaddch(y, x, grid[x + WIDTH*y]);
+			mvaddch(y, x, grid[x][y]);
 		}
 	}
 	mvprintw(LINES-1, 0, "%s", info);
@@ -75,21 +72,21 @@ void display(const char *info){
 /*
  * Randomly initialize the grid
  */
-void init_grid_rand(){
+void init_grid_rand(char grid[][HEIGHT]){
 	int count;
 	// Set all the cells to OFF
 	memset(grid, (int)OFF, HEIGHT*WIDTH);
 	// Start with random cells on
 	srand(time(NULL));
 	for(count = rand() % (HEIGHT*WIDTH/2); count > 0; --count){
-		grid[(rand()%WIDTH) +WIDTH*(rand()%HEIGHT)] = ON;
+		grid[rand()%WIDTH][rand()%HEIGHT] = ON;
 	}
 }
 
 /*
  * Initialize the grid from a file. Return 1 on success 0 on failure.
  */
-int init_grid_file(char *filename){
+int init_grid_file(char grid[][HEIGHT], char *filename){
 	FILE *fp;
 	int c, x = 0, y = 0;
 	if (!(fp = fopen(filename, "r"))){
@@ -100,7 +97,7 @@ int init_grid_file(char *filename){
 	memset(grid, (int)OFF, HEIGHT*WIDTH);
 	while((c = fgetc(fp)) != EOF){
 		if(c == ON)
-			grid[x + WIDTH*y] = ON;
+			grid[x][y] = ON;
 		// increment x and possibly y
 		x++;
 		if(x == WIDTH || c == '\n'){
@@ -126,20 +123,15 @@ int main(int argc, char **argv){
 	timeout(10);
 	noecho();
 
-	grid = (char *)malloc(HEIGHT * WIDTH);
-	if(grid == NULL){
-		endwin();
-		return 1;
-	}
+	char grid[WIDTH][HEIGHT];
 
 	if(argc == 2){
-		if(!init_grid_file(argv[1])){
+		if(!init_grid_file(grid, argv[1])){
 			endwin();
-			free(grid);
 			return 1;
 		}
 	}else{
-		init_grid_rand();
+		init_grid_rand(grid);
 	}
 	delay = DELAY;
 	while((c = getch()) != 'q'){
@@ -152,12 +144,11 @@ int main(int argc, char **argv){
 			delay += 5000;
 
 		if(!paused){
-			display("Press enter to start/stop or 'q' to exit");
-			update_grid();
+			display(grid, "Press enter to start/stop or 'q' to exit");
+			update_grid(grid);
 		}
 	}
 
 	endwin();
-	free(grid);
 	return 0;
 }
